43020de557ee116990d47630bd7c5c694ff104f5
akondo06 <akondo.ionutz@gmail.com>
2016-04-11 20:15:18 +0200

    more stuff on product page

--- header.php
+++ header.php
@@ -60,4 +60,7 @@
 		 *
 		 * @hooked woocommerce_breadcrumb - 10
 		 */
+		if(is_product()) {
+			remove_action( 'storefront_content_top', 'woocommerce_breadcrumb');
+		}
 		do_action( 'storefront_content_top' );
